f6c4f4e1d05e0a5ef4c0a6929dd2f666e297d2c1
dcl525 <dcl525@gmail.com>
2021-06-20 22:42:07 -0700

    adding license headers, code cleanup

--- Services.DesertMusic.Api/Controllers/ShipperController.cs
+++ Services.DesertMusic.Api/Controllers/ShipperController.cs
@@ -1,11 +1,22 @@
-﻿using Common.Utilities.AspNetCore.Response.Extensions;
-using Microsoft.AspNetCore.Http;
+﻿/* Copyright (C) 2012, 2013 Dan Leonard
+ * 
+ * This is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your option) 
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ * for more details.
+ */
+
+
+using Common.Utilities.AspNetCore.Response.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Services.DesertMusic.Api.Components;
 using Services.DesertMusic.Api.Models.Shipper;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Services.DesertMusic.Api.Controllers
--- Services.DesertMusic.Api/Program.cs
+++ Services.DesertMusic.Api/Program.cs
@@ -1,3 +1,17 @@
+/* Copyright (C) 2012, 2013 Dan Leonard
+ * 
+ * This is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your option) 
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ * for more details.
+ */
+
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
